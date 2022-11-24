81d3c6c35862f0014137db0d3b9bf9dc0e13e45a
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-23 22:21:13 -0800

    refactor code

--- fetcher.js
+++ fetcher.js
@@ -15,13 +15,15 @@ const filepathCLI = process.argv[3];
 const writeFile = (fp, c) => {
   fs.writeFile(fp, c, err => {
     
-    if (err) console.log(`Write File Error: ${err}`);
+    if (err) return console.log(`Write File Error: ${err}`);
 
     //log filesize and file path to console after download
     fs.stat(fp, (err, stats) => {
-      if (err) console.log(`FS stat error: ${err}`);
-
+      if (err) return console.log(`FS stat error: ${err}`);
+      
       console.log(`Downloaded and saved ${stats.size} bytes to ${fp}`);
+      rl.close();
+      return;
     });
   });
 };
@@ -42,30 +44,26 @@ const fetcher = (url, filepath) => {
       console.log('statusCode:', response && response.statusCode);
       console.error('Error: Invalid URL!');
       rl.close();
+      return;
+    }
 
-    } else {
+    //checks if file already exist
+    if (fs.existsSync(filepath)) {
+      rl.question('File already exist. Do you want to overwrite? (Y/N) ', (answer) => {
 
-      //checks if file already exist
-      if (fs.existsSync(filepath)) {
-        rl.question('File already exist. Do you want to overwrite? (Y/N) ', (answer) => {
-
-          if (answer.toUpperCase() === 'Y') {
-            writeFile(filepath, body);
-            rl.close();
-
-          } else {
-            console.log('No file downloaded');
-            rl.close();
-          }
-
-        });
+        if (answer.toUpperCase() === 'Y') {
+          writeFile(filepath, body);
+          return;
+        }
+        
+        console.log('No file downloaded');
+        rl.close();
+        return;
+      });
         
       //execute function if file does not exist yet
-      } else {
-        writeFile(filepath, body);
-        rl.close();
-      }
-    }
+    } else return writeFile(filepath, body);
+    
   });
 };
 
